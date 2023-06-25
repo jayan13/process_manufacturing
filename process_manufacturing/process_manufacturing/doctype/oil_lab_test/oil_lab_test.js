a436a9d3d907ea09f443c2cbac56300aa52ce30b
jayan13 <jayakumar13s@gmail.com>
2023-06-25 15:39:24 +0530

    oil lab test

--- process_manufacturing/process_manufacturing/doctype/oil_lab_test/oil_lab_test.js
+++ process_manufacturing/process_manufacturing/doctype/oil_lab_test/oil_lab_test.js
@@ -2,7 +2,29 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Oil Lab Test', {
-	// refresh: function(frm) {
+	 //refresh: function(frm) {
 
-	// }
+		
+	 //},
+	 density_per_sg: function(frm, cdt, cdn) 
+	 { 
+		
+		 var d = locals[cdt][cdn];
+		if(d.density_per_sg && d.net_weight)
+		{
+			 d.qty_in_litters=Math.round(d.net_weight/d.density_per_sg);
+			 frm.refresh_fields();
+		 }
+	 } ,
+	 net_weight: function(frm, cdt, cdn) 
+	 { 
+		
+		 var d = locals[cdt][cdn];
+		if(d.density_per_sg && d.net_weight)
+		{
+			 d.qty_in_litters=Math.round(d.net_weight/d.density_per_sg);
+			 frm.refresh_fields();
+		 }                
+		 
+	 } 
 });
