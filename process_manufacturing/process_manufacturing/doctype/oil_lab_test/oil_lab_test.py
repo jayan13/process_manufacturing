3114c3b56ae6079f451b49ae89474b986cd6c611
jayan13 <jayakumar13s@gmail.com>
2023-06-29 12:05:27 +0530

    labtest bug fix

--- process_manufacturing/process_manufacturing/doctype/oil_lab_test/oil_lab_test.py
+++ process_manufacturing/process_manufacturing/doctype/oil_lab_test/oil_lab_test.py
@@ -7,46 +7,52 @@ from frappe.model.document import Document
 class OilLabTest(Document):
 	def on_update(self):
 		if self.referance_type=='Purchase Receipt':
-			if self.voucher_no:
-				net_weight=frappe.db.get_value('Purchase Receipt',{'name':self.voucher_no},'net_weight') or 0
-				qty_in_litters=0
-				density_per_sg=self.density_per_sg or 0
-				flash_point=self.flash_point or 0
-				water_contant=self.water_contant or 0
-				if net_weight and density_per_sg:
-					qty_in_litters=float(net_weight)/float(density_per_sg)
-				frappe.db.set_value('Purchase Receipt',self.voucher_no,{'density_per_sg':density_per_sg,'flash_point':flash_point,'water_contant':water_contant,'qty_in_litters':qty_in_litters})
+			if self.voucher_no:				
+				if not frappe.db.get_value('Purchase Receipt',{'name':self.voucher_no},'docstatus'):
+					net_weight=frappe.db.get_value('Purchase Receipt',{'name':self.voucher_no},'net_weight') or 0
+					qty_in_litters=0
+					density_per_sg=self.density_per_sg or 0
+					flash_point=self.flash_point or 0
+					water_contant=self.water_contant or 0
+					if net_weight and density_per_sg:
+						qty_in_litters=float(net_weight)/float(density_per_sg)
+					frappe.db.set_value('Purchase Receipt',self.voucher_no,{'density_per_sg':density_per_sg,'flash_point':flash_point,'water_contant':water_contant,'qty_in_litters':qty_in_litters})
+				else:
+					frappe.throw("You cannot update Test report, Purchase Receipt is already submitted")
 		else:
 			if self.voucher_no:
-				net_weight=frappe.db.get_value('Delivery Note',{'name':self.voucher_no},'net_weight') or 0
-				qty_in_litters=0
-				density_per_sg=self.density_per_sg or 0
-				flash_point=self.flash_point or 0
-				water_contant=self.water_contant or 0
-				if net_weight and density_per_sg:
-					qty_in_litters=float(net_weight)/float(density_per_sg)				
-				frappe.db.set_value('Delivery Note',self.voucher_no,{'density_per_sg':density_per_sg,'flash_point':flash_point,'water_contant':water_contant,'qty_in_litters':qty_in_litters})
-
+				if not frappe.db.get_value('Delivery Note',{'name':self.voucher_no},'docstatus'):
+					net_weight=frappe.db.get_value('Delivery Note',{'name':self.voucher_no},'net_weight') or 0
+					qty_in_litters=0
+					density_per_sg=self.density_per_sg or 0
+					flash_point=self.flash_point or 0
+					water_contant=self.water_contant or 0
+					if net_weight and density_per_sg:
+						qty_in_litters=float(net_weight)/float(density_per_sg)				
+					frappe.db.set_value('Delivery Note',self.voucher_no,{'density_per_sg':density_per_sg,'flash_point':flash_point,'water_contant':water_contant,'qty_in_litters':qty_in_litters})
+				else:
+					frappe.throw("You cannot update Test report, Delivery Note is already submitted")
 
 @frappe.whitelist()
 def update_test_purchase(doc, method):
 	if doc.company=='Dure Oil Middle East Factory - Sole Proprietorship LLC':
 		lbrest=frappe.db.get_value('Oil Lab Test',{'voucher_no':doc.name,'referance_type':'Purchase Receipt'},'name')
-		if lbrest:
-			tdoc=frappe.get_doc("Oil Lab Test", lbrest)
-			tdoc.tickect_no=doc.tickect_no			
-			tdoc.party_type='Supplier'
-			tdoc.party=doc.supplier
-			tdoc.save()
-		else:
-			tdoc=frappe.new_doc("Oil Lab Test")
-			tdoc.tickect_no=doc.tickect_no
-			tdoc.referance_type='Purchase Receipt'
-			tdoc.voucher_no=doc.name
-			tdoc.party_type='Supplier'
-			tdoc.party=doc.supplier
-			tdoc.save()
-			frappe.db.set_value('Purchase Receipt',doc.name,{'ltr_no':tdoc.name})
+		if doc.docstatus=='0':
+			if lbrest:
+				tdoc=frappe.get_doc("Oil Lab Test", lbrest)
+				tdoc.tickect_no=doc.tickect_no			
+				tdoc.party_type='Supplier'
+				tdoc.party=doc.supplier
+				tdoc.save()
+			else:
+				tdoc=frappe.new_doc("Oil Lab Test")
+				tdoc.tickect_no=doc.tickect_no
+				tdoc.referance_type='Purchase Receipt'
+				tdoc.voucher_no=doc.name
+				tdoc.party_type='Supplier'
+				tdoc.party=doc.supplier
+				tdoc.save()
+				frappe.db.set_value('Purchase Receipt',doc.name,{'ltr_no':tdoc.name})
 
 		
 
@@ -54,21 +60,22 @@ def update_test_purchase(doc, method):
 def update_test_delivary(doc, method):
 	if doc.company=='Dure Oil Middle East Factory - Sole Proprietorship LLC':
 		lbrest=frappe.db.get_value('Oil Lab Test',{'voucher_no':doc.name,'referance_type':'Delivery Note'},'name')
-		if lbrest:
-			tdoc=frappe.get_doc("Oil Lab Test", lbrest)
-			tdoc.tickect_no=doc.tickect_no			
-			tdoc.party_type='Customer'
-			tdoc.party=doc.customer
-			tdoc.save()
-			
-		else:
-			tdoc=frappe.new_doc("Oil Lab Test")
-			tdoc.tickect_no=doc.tickect_no
-			tdoc.referance_type='Delivery Note'
-			tdoc.voucher_no=doc.name
-			tdoc.party_type='Customer'
-			tdoc.party=doc.customer
-			tdoc.save()
-			frappe.db.set_value('Delivery Note',doc.name,{'ltr_no':tdoc.name})
+		if doc.docstatus=='0':
+			if lbrest:
+				tdoc=frappe.get_doc("Oil Lab Test", lbrest)
+				tdoc.tickect_no=doc.tickect_no			
+				tdoc.party_type='Customer'
+				tdoc.party=doc.customer
+				tdoc.save()
+				
+			else:
+				tdoc=frappe.new_doc("Oil Lab Test")
+				tdoc.tickect_no=doc.tickect_no
+				tdoc.referance_type='Delivery Note'
+				tdoc.voucher_no=doc.name
+				tdoc.party_type='Customer'
+				tdoc.party=doc.customer
+				tdoc.save()
+				frappe.db.set_value('Delivery Note',doc.name,{'ltr_no':tdoc.name})
 
 		
